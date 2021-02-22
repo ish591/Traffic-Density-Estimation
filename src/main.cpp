3f8f04ddede0c7a4b902ad80ebdd017e046e691f
Ishaan Singh <ishaansingh@Ishaans-MacBook-Air.local>
2021-02-22 15:48:52 +0530

    sorted points

--- src/main.cpp
+++ src/main.cpp
@@ -4,16 +4,36 @@
 using namespace std;
 using namespace cv;
 
-vector<Point2f> pts_src, pts_dst;
-
+vector<pair<int,int>> temp;
 void clickEvent(int event, int x, int y, int flags, void *params)
 {
-	if(event == EVENT_LBUTTONDOWN)
-	{
-		cout << "(" << x << ", " << y << ")\n";
-        pts_src.push_back(Point2f(x, y));
-	}
+    if(event == EVENT_LBUTTONDOWN)
+    {
+        cout << "(" << x << ", " << y << ")\n";
+        temp.push_back({x,y});
+    }
 }
+
+
+vector<Point2f> sortpoints(){
+    vector<Point2f> pts_src;
+    sort(temp.begin(),temp.end());
+    if(temp[0].second>temp[1].second){
+        pair<int,int>u=temp[0];
+        temp[0]=temp[1];
+        temp[1]=u;
+    }
+    if(temp[3].second>temp[2].second){
+           pair<int,int>u=temp[2];
+           temp[2]=temp[3];
+           temp[3]=u;
+    }
+    for (int i=0;i<4;i++){
+        pts_src.push_back(Point2f(temp[i].first,temp[i].second));
+    }
+    return pts_src;
+}
+
 
 int main(int argc, char* argv[])
 {
@@ -24,19 +44,24 @@ int main(int argc, char* argv[])
     
     setMouseCallback("Empty", clickEvent);
 
-    while(pts_src.size() < 4)
+    while(temp.size() < 4)
     {
         waitKey(200);
     }
-    destroyWindow("Empty");
 
+    destroyWindow("Empty");
+    vector<Point2f> pts_src, pts_dst;
+    pts_src=sortpoints();
+    
+    waitKey(100);
+    
     // change to calculate dynamically
     pts_dst.push_back(Point2f(472,52));
     pts_dst.push_back(Point2f(472, 830));
     pts_dst.push_back(Point2f(800, 830));
-	pts_dst.push_back(Point2f(800,52));
+    pts_dst.push_back(Point2f(800,52));
     
-
+    cout<<im_empty.size()<<endl;
     Mat homography = findHomography(pts_src, pts_dst);
 
     Mat im_empty_warped, im_empty_cropped, im_traffic_warped, im_traffic_cropped;
@@ -49,7 +74,7 @@ int main(int argc, char* argv[])
     destroyWindow("Warped");
 
     Rect crop_coordinates = Rect(472, 52, 328, 778); // make crop coordinates dynamic
-    im_empty_cropped = im_empty_warped(crop_coordinates); 
+    im_empty_cropped = im_empty_warped(crop_coordinates);
     im_traffic_cropped = im_traffic_warped(crop_coordinates);
     
     imshow("Cropped", im_empty_cropped);
@@ -61,5 +86,6 @@ int main(int argc, char* argv[])
     imwrite("traffic_warped.jpg", im_traffic_warped);
     imwrite("traffic_cropped.jpg", im_traffic_cropped);
 
-	return 0;
+    return 0;
 }
+
